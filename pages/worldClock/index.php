594403d3c75a1536dd78d067d432c514df659342
a-manalo <agmnlo.by@gmail.com>
2025-05-17 22:40:25 +0800

    feat: add background image to world clock page

--- pages/worldClock/index.php
+++ pages/worldClock/index.php
@@ -12,6 +12,11 @@ head('World Digital Clock');
 navHeader();
 ?>
 
+<!-- background image -->
+<div style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -1;">
+    <img src=".../../assets/img/ghiblimovies.jpg" style="width: 100%; height: 100%; object-fit: cover; opacity: 0.8">
+</div>
+
 <?php
 footer();
 ?>
