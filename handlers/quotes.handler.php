3c4a4b7a5eaac9c75165a3ad2970b9bf42470982
a-manalo <agmnlo.by@gmail.com>
2025-05-17 21:14:49 +0800

    feat: add ghibli quotes data handler

---
+++ handlers/quotes.handler.php
@@ -0,0 +1,19 @@
+<?php
+
+$ghibliQuotes = [
+    [
+        'quote' => 'Life is a winking light in the darkness.',
+        'movie' => 'Grave of the Fireflies',
+        'year' => 1988
+    ],
+    [
+        'quote' => 'Life is suffering. It is hard. The world is cursed. But still, you find reasons to keep living.',
+        'movie' => 'Princess Mononoke',
+        'year' => 1997
+    ],
+    [
+        'quote' => 'Many of the things that seem impossible now will become realities tomorrow.',
+        'movie' => 'Howl\'s Moving Castle',
+        'year' => 2004
+    ]
+];
