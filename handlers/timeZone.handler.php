51e9eb153314326ce87826d4f96f297efdcf8fd7
a-manalo <agmnlo.by@gmail.com>
2025-05-18 00:09:37 +0800

    feat: added data handler for time zones

---
+++ handlers/timeZone.handler.php
@@ -0,0 +1,74 @@
+<?php
+
+$timeZones = [
+    [
+        'city' => 'Manila',
+        'country' => 'Philippines',
+        'timezone' => 'Asia/Manila',
+        'offset' => '+08:00',
+        'abbr' => 'PHT'
+    ],
+    [
+        'city' => 'Tokyo',
+        'country' => 'Japan',
+        'timezone' => 'Asia/Tokyo',
+        'offset' => '+09:00',
+        'abbr' => 'JST'
+    ],
+    [
+        'city' => 'New York',
+        'country' => 'United States',
+        'timezone' => 'America/New_York',
+        'offset' => '-05:00',
+        'abbr' => 'EST'
+    ],
+    [
+        'city' => 'London',
+        'country' => 'United Kingdom',
+        'timezone' => 'Europe/London',
+        'offset' => '+00:00',
+        'abbr' => 'GMT'
+    ],
+    [
+        'city' => 'Sydney',
+        'country' => 'Australia',
+        'timezone' => 'Australia/Sydney',
+        'offset' => '+10:00',
+        'abbr' => 'AEST'
+    ],
+    [
+        'city' => 'Dubai',
+        'country' => 'United Arab Emirates',
+        'timezone' => 'Asia/Dubai',
+        'offset' => '+04:00',
+        'abbr' => 'GST'
+    ],
+    [
+        'city' => 'Paris',
+        'country' => 'France',
+        'timezone' => 'Europe/Paris',
+        'offset' => '+01:00',
+        'abbr' => 'CET'
+    ],
+    [
+        'city' => 'Singapore',
+        'country' => 'Singapore',
+        'timezone' => 'Asia/Singapore',
+        'offset' => '+08:00',
+        'abbr' => 'SGT'
+    ],
+    [
+        'city' => 'Los Angeles',
+        'country' => 'United States',
+        'timezone' => 'America/Los_Angeles',
+        'offset' => '-08:00',
+        'abbr' => 'PST'
+    ],
+    [
+        'city' => 'Seoul',
+        'country' => 'South Korea',
+        'timezone' => 'Asia/Seoul',
+        'offset' => '+09:00',
+        'abbr' => 'KST'
+    ]
+];
