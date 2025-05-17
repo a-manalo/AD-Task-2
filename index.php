8aebb845bbfd0274947e13d0c63b0fcb03278e08
a-manalo <agmnlo.by@gmail.com>
2025-05-17 21:20:14 +0800

    feat: add quote display section and integrate updateQuote function

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
 require BASE_PATH . '/utils/htmlEscape.utils.php';
+require_once BASE_PATH . '/handlers/quotes.handler.php';
+require_once BASE_PATH . '/utils/updateQuote.utils.php';
 
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
@@ -19,6 +21,16 @@ navHeader();
         Your browser does not support the video tag.
     </video>
 </div>
+
+<!-- display ghibli quotes -->
+<div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; width: 80%; max-width: 800px;">
+    <div id="quote-container" style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 2);">
+        <p id="quote-text" style="font-size: 2em; margin-bottom: 1rem; font-style: italic;"></p>
+        <p id="quote-source" style="font-size: 1.2em; opacity: 1;"></p>
+    </div>
+</div>
+
+<?php echo updateQuote(); ?>
 
 <?php
 footer();
